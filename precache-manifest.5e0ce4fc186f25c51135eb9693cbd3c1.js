7d4220bc97c09a5e6a251bc3c56a92a7695d08c1
olekscode <olk.zaytsev@gmail.com>
2020-04-06 01:58:07 +0200

    Updates

---
+++ precache-manifest.5e0ce4fc186f25c51135eb9693cbd3c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e753b78f47f3469c348c685b83852366",
+    "url": "/oleks.fr.react/index.html"
+  },
+  {
+    "revision": "f0fc81388b8163e465e4",
+    "url": "/oleks.fr.react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e54ebacb9a55186163b",
+    "url": "/oleks.fr.react/static/js/2.9cb22827.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oleks.fr.react/static/js/2.9cb22827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0fc81388b8163e465e4",
+    "url": "/oleks.fr.react/static/js/main.3b809873.chunk.js"
+  },
+  {
+    "revision": "90d1328cb28e3f81531e",
+    "url": "/oleks.fr.react/static/js/runtime-main.2d4244d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oleks.fr.react/static/media/logo.5d5d9eef.svg"
+  }
+]);
