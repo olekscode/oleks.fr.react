d1ef478cfc8f09e32b1e2094cc1ad73763b08c95
olekscode <olk.zaytsev@gmail.com>
2020-04-06 02:57:18 +0200

    Updates

--- precache-manifest.ba72d2c85cc3b72adcfb93fdd2148629.js
+++ precache-manifest.ba72d2c85cc3b72adcfb93fdd2148629.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1f0f7a56da5026be40c42c7e738ad50a",
+    "revision": "83fca8417c05efe64e9ff2fc7b2afd7f",
     "url": "/oleks.fr.react/index.html"
   },
   {
-    "revision": "06da4aef09861850f231",
+    "revision": "923dbf20475cd63050d6",
     "url": "/oleks.fr.react/static/css/main.a9d946fc.chunk.css"
   },
   {
-    "revision": "0f2d84845f49ab037975",
-    "url": "/oleks.fr.react/static/js/2.e7fa2dc8.chunk.js"
+    "revision": "68f48a9c4c583c350174",
+    "url": "/oleks.fr.react/static/js/2.521139be.chunk.js"
   },
   {
-    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/oleks.fr.react/static/js/2.e7fa2dc8.chunk.js.LICENSE.txt"
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oleks.fr.react/static/js/2.521139be.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "06da4aef09861850f231",
-    "url": "/oleks.fr.react/static/js/main.32f10d0a.chunk.js"
+    "revision": "923dbf20475cd63050d6",
+    "url": "/oleks.fr.react/static/js/main.cadda94e.chunk.js"
   },
   {
     "revision": "90d1328cb28e3f81531e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oleks.fr.react/precache-manifest.99828213ed2d70abcf4269755b06994b.js"
+  "/oleks.fr.react/precache-manifest.ba72d2c85cc3b72adcfb93fdd2148629.js"
 );
 
 self.addEventListener('message', (event) => {
